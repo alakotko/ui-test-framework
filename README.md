384192d7ddac87d48ed96654b82b25748710b63d
alakotko <ejakaa@gmail.com>
2023-08-30 11:28:48 +0200

    done

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ui-test-framework
-Framework for UI Testing For Playwright
+Framework for Testing With Playwright Test as a test-runner
 
-# Instalation
+# 1. Instalation
 For instalation you need:
 - Node.js 18.7.1 LTS +
 
-## 1. Install packages
+## 1.1 Install packages
 **npm:**
 ```npm install```
 **pnpm**
@@ -13,5 +13,29 @@ For instalation you need:
 **yarn**
 ```yarn```
 
-## 2. Run a test
+## 1.2 Install playwright browsers
+Playwright can install supported browsers. Running the command without arguments will install the default browsers.
+```npx playwright install```
+
+You can also install specific browsers by providing an argument:
+```npx playwright install webkit```
+
+See all supported browsers:
+```npx playwright install --help```
+
+
+## 1.3 Run test
 ```npx playwright test```
+
+# 2. Configuration
+All configuration based on config(https://www.npmjs.com/package/config) package.
+This is mean you need add your env config to ./config folder and you can access it everywhere by importing it
+```import config from 'config'```
+
+Project get configuration based on **NODE_ENV**, it take it value and use it as a config path, for example:
+```export NODE_ENV=production```
+```npx playwright test```
+lead to config search for ./config/production.json
+
+In case if theres no such file - it uses default.json.
+
---
+++ tests/ui/widget.spec.js
@@ -0,0 +1,54 @@
+import { test } from "@playwright/test";
+import { DonationWidget } from "../../lib/pages/widgets/donationWidget";
+import { DonationPage } from "../../lib/pages/donationPage";
+
+test("Payment / Widget / Card '4242 4242 4242 4242' / Should throw a error ", async ({
+  page,
+}) => {
+  const paymentData = {
+    amount: "100",
+    currency: "USD",
+    currencySymbol: "$",
+  };
+  const cardData = {
+    cardNum: "4242 4242 4242 4242",
+    expDate: "04 / 24",
+    cvv: "000",
+  };
+  const personalData = {
+    firstName: "First Name",
+    lastName: "Last Name",
+    email: "alakotko@gmail.com",
+  };
+
+  const donationPage = new DonationPage(page);
+  const donationWidget = new DonationWidget(page);
+
+  await donationPage.goto();
+  await donationPage.giveNowButtonClick();
+
+  await donationWidget.displayed();
+  await donationWidget.choiceMonthlyPayments();
+  await donationWidget.addAmountAndCurrency(
+    paymentData.amount,
+    paymentData.currency,
+    paymentData.currencySymbol
+  );
+  await donationWidget.donateButtonClick();
+
+  await donationWidget.deselectCoverTransaction();
+  await donationWidget.clickCreditCard();
+  await donationWidget.fillCardData(
+    cardData.cardNum,
+    cardData.expDate,
+    cardData.cvv
+  );
+  await donationWidget.fillPersonalData(
+    personalData.firstName,
+    personalData.lastName,
+    personalData.email
+  );
+  await donationWidget.privacyContinueClick();
+
+  await donationWidget.checkErrorAppears();
+});
